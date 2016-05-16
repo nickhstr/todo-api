1e000ef7cd764de20b8833f904f1a76c6d1bf9ef
Nick Hester <nickhstr@gmail.com>
2016-05-16 15:46:56 -0700

    Create query search

--- server.js
+++ server.js
@@ -15,7 +15,23 @@ app.get('/', function(req, res) {
 
 // GET /todos
 app.get('/todos', function(req, res) {
-	res.json(todos);
+	let queryParams = req.query;
+	let filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	}
+	else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
